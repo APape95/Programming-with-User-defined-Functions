bf4cbab91d23b82e231faa7ec8ae74d1b4e6aa4d
Amy Pape <A-C.1995@outlook.com>
2024-02-25 19:08:44 +0000

    Update holiday.py
    correcting names of variables for clarity

--- holiday.py
+++ holiday.py
@@ -30,45 +30,45 @@ while True:
     
     if user_choice == '1':
         # This is the cost of the plane ticket pp.
-        plane_cost = 100
+        city_flight = 100
         # Lets the user know we have their selection.
         print("Spain selected")
-        print(f"Your flights will cost £{plane_cost} per person")
+        print(f"Your flights will cost £{city_flight} per person")
         # Calculate the total cost relative to number of people.
-        ttl_plane = plane_cost * num_of_people
-        print(f"This comes to £{ttl_plane} for everyone")
+        plane_cost = city_flight * num_of_people
+        print(f"This comes to £{plane_cost} for everyone")
         break
 
     elif user_choice == '2':
-        plane_cost = 200
+        city_flight = 200
         print("Germany selected.")
-        print(f"Your flights will cost £{plane_cost} per person")
-        ttl_plane = plane_cost * num_of_people
-        print(f"This comes to £{ttl_plane} for everyone")
+        print(f"Your flights will cost £{city_flight} per person")
+        plane_cost = city_flight * num_of_people
+        print(f"This comes to £{plane_cost} for everyone")
         break
 
     elif user_choice == '3':
-        plane_cost = 300
+        city_flight = 300
         print("Portugal selected.")
-        print(f"Your flights will cost £{plane_cost} per person")
-        ttl_plane = plane_cost * num_of_people
-        print(f"This comes to £{ttl_plane} for everyone")
+        print(f"Your flights will cost £{city_flight} per person")
+        plane_cost = city_flight * num_of_people
+        print(f"This comes to £{plane_cost} for everyone")
         break
 
     elif user_choice == '4':
-        plane_cost = 400
+        city_flight = 400
         print("Greece selected.")
-        print(f"Your flights will cost £{plane_cost} per person")
-        ttl_plane = plane_cost * num_of_people
-        print(f"This comes to £{ttl_plane} for everyone")
+        print(f"Your flights will cost £{city_flight} per person")
+        plane_cost = city_flight * num_of_people
+        print(f"This comes to £{plane_cost} for everyone")
         break
 
     elif user_choice == '5':
-        plane_cost = 500
+        city_flight = 500
         print("France selected.")
-        print(f"Your flights will cost £{plane_cost} per person")
-        ttl_plane = plane_cost * num_of_people
-        print(f"This comes to £{ttl_plane} for everyone")
+        print(f"Your flights will cost £{city_flight} per person")
+        plane_cost = city_flight * num_of_people
+        print(f"This comes to £{plane_cost} for everyone")
         break
 
     # If the user selects to exit we break the loop.
@@ -92,15 +92,15 @@ print(f"Your hotel for {num_nights} nights will cost £{hotel_pp} per person")
 print(f"This will be {hotel_cost} total")
 
 # Ask the user how many days car rental they want. Store as variable.
-rent_days = int(input("How many days car rental do you want?"))
+rental_days = int(input("How many days car rental do you want?"))
 # The rental daily cost is £100.
 # Work out the rental cost by mulitplying rental days by daily cost.
-rent_cost = rent_days * 150
-print(f"Your rental for {rent_days} days will cost £{rent_cost}")
+car_rental = rental_days * 150
+print(f"Your rental for {rental_days} days will cost £{car_rental}")
 
 # Work out the total cost of the trip by adding flights, hotel and car charges.
-holiday_cost = plane_cost + hotel_cost + rent_cost
-print(f"Your entire holiday will cost £{holiday_cost} for {num_of_people} of you.")
+holiday_cost = plane_cost + hotel_cost + car_rental
+print(f"Your entire holiday will cost £{holiday_cost:.2f} for {num_of_people} of you.")
 # Calculate a per person price by dividing the total cost by num people.
 holiday_pp = holiday_cost / num_of_people
-print(f"This would be £{holiday_pp} per person.")
+print(f"This would be £{holiday_pp:.2f} per person.")
